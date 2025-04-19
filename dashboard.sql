a330177a5146a7f2c01d78a3c505a7659a19f8e7
Sarra-Firn <sarrafirn@gmail.com>
2025-04-19 21:29:48 +0300

    Add files via upload

--- dashboard.sql
+++ dashboard.sql
@@ -164,5 +164,7 @@ SELECT
     ) AS prc
 FROM sessions AS s
 LEFT JOIN leads AS l
-	ON s.visitor_id = l.visitor_id
-	AND s.visit_date <= l.created_at; -- Только лиды после визита
+ON 
+	s.visitor_id = l.visitor_id
+AND 
+	s.visit_date <= l.created_at; -- Только лиды после визита
